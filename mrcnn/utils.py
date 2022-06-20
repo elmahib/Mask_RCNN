f9ee08c554891e759cb78ebf9db3888c4969bcad
elmahib <106749278+elmahib@users.noreply.github.com>
2022-06-20 13:37:39 +0200

    Update utils.py

--- mrcnn/utils.py
+++ mrcnn/utils.py
@@ -356,9 +356,14 @@ class Dataset(object):
         """Load the specified image and return a [H,W,3] Numpy array.
          """
          # Load image
-        image = skimage.io.imread(self.image_info[image_id]['path'])         
-         #image = image[..., np.newaxis] # Extending the size of the image to be (h,w,1)
-        return image
+        image = skimage.io.imread(self.image_info[image_id]['path']) 
+        # Convert to grayscale for consistency.        
+        if image.ndim != 1:
+             image = skimage.color.gray2rgb(image) #Instead of rgb2gray(image)
+ 
+         # Extending the size of the image to be (h,w,1)
+         image = image[..., np.newaxis]
+         return image
 
     def load_mask(self, image_id):
         """Load instance masks for the given image.
