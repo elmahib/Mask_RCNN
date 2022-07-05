eda3523436c11d9d6cf57c4095dc6fd88db92c6c
elmahib <106749278+elmahib@users.noreply.github.com>
2022-07-05 11:15:00 +0200

    Update config.py

--- mrcnn/config.py
+++ mrcnn/config.py
@@ -1,7 +1,6 @@
 """
 Mask R-CNN
 Base Configurations class.
-
 Copyright (c) 2017 Matterport, Inc.
 Licensed under the MIT License (see LICENSE for details)
 Written by Waleed Abdulla
@@ -134,10 +133,10 @@ class Config(object):
     # Number of color channels per image. RGB = 3, grayscale = 1, RGB-D = 4
     # Changing this requires other changes in the code. See the WIKI for more
     # details: https://github.com/matterport/Mask_RCNN/wiki
-    IMAGE_CHANNEL_COUNT = 1
+    IMAGE_CHANNEL_COUNT = 3
 
     # Image mean (RGB)
-    MEAN_PIXEL = np.array([123.7])
+    MEAN_PIXEL = np.array([123.7, 116.8, 103.9])
 
     # Number of ROIs per image to feed to classifier/mask heads
     # The Mask RCNN paper uses 512 but often the RPN doesn't generate
